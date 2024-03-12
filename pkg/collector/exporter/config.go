712c932bb33975e22220bb66fd068a270d226653
chenjiandongx <chenjiandongx@qq.com>
2024-03-12 23:15:37 +0800

    chore: 细节调整

--- pkg/collector/exporter/config.go
+++ pkg/collector/exporter/config.go
@@ -22,7 +22,7 @@ const (
 	defaultMetricsBatchSize = 2000
 	defaultTracesBatchSize  = 200
 	defaultLogsBatchSize    = 100
-	defaultProxyBatchSize   = 1000
+	defaultProxyBatchSize   = 2000
 	defaultFlushInterval    = 3 * time.Second
 )
 
